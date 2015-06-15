b9f38cd770be078b2c68f56641ec2f0f3094b8f7
Alexey Osipenko <alexey@osipenko.in.ua>
2015-06-15 11:13:31 +0300

    Fix synonym set for the icons

--- lib/purpur/helpers.rb
+++ lib/purpur/helpers.rb
@@ -8,7 +8,7 @@ module Purpur
       content_tag(:span, {class: styleclass, data: { icon: Purpur.icon_name(name), 'icon-size' => size }}.deep_merge(options)) do
         content_tag(:span, class: 'icon--wrapper') do
           content_tag(:svg, class: 'icon--cnt') do
-            content_tag('use', nil, 'xlink:href' => "#{asset_url('purpur.svg')}##{name}-icon", 'x' => 0, 'y' => 0)
+            content_tag('use', nil, 'xlink:href' => "#{asset_url('purpur.svg')}##{Purpur.icon_name(name)}-icon", 'x' => 0, 'y' => 0)
           end
         end
       end
